33810e2b713f51c77f9e941a8ef5a13b7ce32d9e
byomkesh462 <88849299+byomkesh462@users.noreply.github.com>
2022-03-12 12:41:03 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,11 +11,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
 @bot.on_message(filters.group | filters.channel | filters.new_chat_members)
-async def NewChat(bot, message):
+def NewChat(bot, message):
     logging.info("New chat {}".format(message.chat.id))
     logging.info("Getting memebers from {}".format(message.chat.id))
-    new_members = await message.new_chat_members
-    admins = await bot.get_chat_members(message.chat.id, filter="administrators") 
+    new_members = message.new_chat_members
+    admins = bot.get_chat_members(message.chat.id, filter="administrators") 
     for new_mem in new_members:
         if new_mem in admins:
             logging.info("{} is an admin in {}".format(new_mem.user.id,message.chat.id))
@@ -24,7 +24,7 @@ async def NewChat(bot, message):
             continue
         else:
             try:
-                await bot.get_chat_member(-1001513657958, new_mem.user.id)
+                bot.get_chat_member(-1001513657958, new_mem.user.id)
                 logging.info("{} is a member in {}".format(new_mem.user.id,message.chat.id))
             except Exception:
                 bot.kick_chat_member(chat_id =message.chat.id,user_id=new_mem.user.id)
