e9fa73c80785d3eaee09a02e23eb3ce42cd66385
byomkesh462 <88849299+byomkesh462@users.noreply.github.com>
2022-03-12 11:39:48 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,4 @@
-
+# @ Ayusman Bieb
 from pyrogram import Client,filters
 from .config import Config
 import logging
@@ -10,16 +10,23 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
 
-@bot.on_message(filters.group)
-def NewChat(bot,message):
-    logging.info("new chat {}".format(message.chat.id))
-    logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
-    for i in a:
-        try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
-            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
-        except Exception:
-            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+@bot.on_message(filters.group | filters.channel | filters.new_chat_members)
+def NewChat(bot, message):
+    logging.info("New chat {}".format(message.chat.id))
+    logging.info("Getting memebers from {}".format(message.chat.id))
+    new_members = message.new_chat_members
+    admins = bot.get_chat_members(message.chat.id, filter="administrators")
+    for new_mem in new_members:
+        if new_mem in admins:
+            logging.info("{} is an admin in {}".format(new_mem.user.id,message.chat.id))
+            continue
+        elif new_mem.id == bot.id:
+            continue
+        else:
+            try:
+                bot.kick_chat_member(chat_id =message.chat.id,user_id=new_mem.user.id)
+                logging.info("kicked {} from {}".format(new_mem.user.id,message.chat.id))
+            except Exception:
+                logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
             
     logging.info("process completed")
